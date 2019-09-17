453f2a6ac838baa89b02da27eb7d1012db78d9ae
Leonardo Marchetto Rivaldo <leonardo.marchettorivaldo@altran.com>
2019-09-17 16:59:41 +0100

    git pages

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const entry = {
 };
 const output = {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     publicPath: '/'
 };
 const plugins =  [
